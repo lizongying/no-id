08ff1af69e206e24aa005a0c797cfd72e9da3599
lizongying <lizongying@msn.com>
2019-11-18 18:39:45 +0800

    支持抖音

--- src/js/background.js
+++ src/js/background.js
@@ -1,39 +1,86 @@
 // notify
 const notify = (data) => {
-    const popup = chrome.extension.getViews({type: 'popup'})[0];
-    console.log(popup.$ui[data[0]]);
-    popup.$ui[data[0]](...data.slice(1));
+  const popup = chrome.extension.getViews({type: 'popup'})[0];
+  popup.$ui[data[0]](...data.slice(1));
 };
 
 // Kuaishou
 const getKuaishou = (value) => {
-    fetch(value.split('?', 1)[0], {
-        headers: {
-            'content-type': 'text/html;',
-        }
-    }).then(function (response) {
-        return response.text()
-    }).then(function (text) {
-        const re = /"userId":"(\d+?)"/;
-        const result = text.match(re);
-        if (result) {
-            console.log(result[1]);
-            notify(['setKuaishou', result[1]]);
-        }
-    })
+  if (!value.match(/kuaishou/)) {
+    notify(['setUserId', '']);
+    return;
+  }
+  fetch(value.split('?', 1)[0], {
+    headers: {
+      'content-type': 'text/html;',
+    },
+  }).then(function(response) {
+    return response.text();
+  }).then(function(text) {
+    const re = /"userId":"(\d+?)"/;
+    const result = text.match(re);
+    if (result) {
+      console.log(result[1]);
+      notify(['setUserId', result[1]]);
+    } else {
+      notify(['setUserId', '']);
+    }
+  });
 };
 
 // Xiaohongshu
 const getXiaohongshu = (value) => {
-    const re = /www\.xiaohongshu\.com\/user\/profile\/(.+?)\?/;
-    const result = value.match(re);
+  const re = /www\.xiaohongshu\.com\/user\/profile\/(.+?)\?/;
+  const result = value.match(re);
+  if (result) {
+    console.log(result[1]);
+    notify(['setUserId', result[1]]);
+  } else {
+    notify(['setUserId', '']);
+  }
+};
+
+// Douyin
+const getDouyin = (value) => {
+  if (!value.match(/douyin/)) {
+    notify(['setUserId', '']);
+    return;
+  }
+  fetch(value.split('?', 1)[0], {
+    headers: {
+      'content-type': 'text/html;',
+    },
+  }).then(function(response) {
+    return response.url;
+  }).then(function(text) {
+    const re = /user\/(\d+?)\?/;
+    const result = text.match(re);
     if (result) {
-        console.log(result[1]);
-        notify(['setXiaohongshu', result[1]]);
+      console.log(result[1]);
+      notify(['setUserId', result[1]]);
+    } else {
+      notify(['setUserId', '']);
     }
+  });
+};
+
+const getUserId = (type, value) => {
+  console.log(type, value);
+  switch (type) {
+    case 'kuaishou':
+      getKuaishou(value);
+      break;
+    case 'xiaohongshu':
+      getXiaohongshu(value);
+      break;
+    case 'douyin':
+      getDouyin(value);
+      break;
+    default:
+      break;
+  }
 };
 
 window.bg = {
-    getKuaishou: getKuaishou,
-    getXiaohongshu: getXiaohongshu,
+  getUserId: getUserId,
 };
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,29 +1,22 @@
-import '../css/popup.css'
+import '../css/popup.css';
 
 const bg = chrome.extension.getBackgroundPage().bg;
 
-const setKuaishou = (value) => {
-    const label_kuaishou = document.querySelector('#kuaishou—label');
-    label_kuaishou.value = value;
-};
-
-const setXiaohongshu = (value) => {
-    const label_xiaohongshu = document.querySelector('#xiaohongshu-label');
-    label_xiaohongshu.value = value;
+const setUserId = (value) => {
+  const labelUserId = document.querySelector('#user-id');
+  labelUserId.value = value;
 };
 
 // ui
 window.$ui = {
-    setKuaishou: setKuaishou,
-    setXiaohongshu: setXiaohongshu,
+  setUserId: setUserId,
 };
 
-const input_kuaishou = document.querySelector('#kuaishou');
-input_kuaishou.onchange = (() => {
-    bg.getKuaishou(input_kuaishou.value);
-});
-
-const input_xiaohongshu = document.querySelector('#xiaohongshu');
-input_xiaohongshu.onchange = (() => {
-    bg.getXiaohongshu(input_xiaohongshu.value);
+const input = document.querySelectorAll('input');
+input.forEach((v) => {
+  v.onchange = (() => {
+    const inputType = document.querySelector('input[name="type"]:checked');
+    const inputUrl = document.querySelector('#url');
+    bg.getUserId(inputType.value, inputUrl.value);
+  });
 });
