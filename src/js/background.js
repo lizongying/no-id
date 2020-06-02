ae6f93715a146cc04ef5faf379ca5230530181a8
lizongying <lizongying@msn.com>
2020-06-02 17:02:56 +0800

    增加源

--- src/js/background.js
+++ src/js/background.js
@@ -1,109 +1,167 @@
 // notify
 const notify = (data) => {
-  const popup = chrome.extension.getViews({type: 'popup'})[0];
-  popup.$ui[data[0]](...data.slice(1));
+    const popup = chrome.extension.getViews({type: 'popup'})[0];
+    popup.$ui[data[0]](...data.slice(1));
 };
 
 // Kuaishou
-const getKuaishou = (value) => {
-  if (!value.match(/kuaishou/)) {
-    notify(['setUserId', '']);
-    return;
-  }
-  let url ='https://live.kuaishou.com/graphql';
-  let data = {"operationName": "sensitiveUserInfoQuery",
-    "variables": {"principalId": value.split('?', 1)[0].split('/')[4]},
-    "query": "query sensitiveUserInfoQuery($principalId: String) {sensitiveUserInfo(principalId: $principalId) {userId}}"};
-  console.log(data);
-  fetch(url, {
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(data),
-    method: 'POST',
-  }).then(function(response) {
-    return response.text();
-  }).then(function(text) {
-    console.log(text);
-    const re = /"userId":"(\d+?)"/;
-    const result = text.match(re);
-    if (result) {
-      console.log(result[1]);
-      notify(['setUserId', result[1]]);
-    } else {
-      notify(['setUserId', '']);
+const getKuaishou = (query) => {
+    if (!query.match(/kuaishou/)) {
+        notify(['setUserId', '']);
+        return;
     }
-  });
+    let url = 'https://live.kuaishou.com/graphql';
+    let data = {
+        'operationName': 'sensitiveUserInfoQuery',
+        'variables': {'principalId': query.split('?', 1)[0].split('/')[4]},
+        'query': 'query sensitiveUserInfoQuery($principalId: String) {sensitiveUserInfo(principalId: $principalId) {userId}}',
+    };
+    console.log(data);
+    fetch(url, {
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        method: 'POST',
+    }).then(function(response) {
+        return response.text();
+    }).then(function(text) {
+        console.log(text);
+        const re = /"userId":"(\d+?)"/;
+        const result = text.match(re);
+        if (result) {
+            console.log(result[1]);
+            notify(['setUserId', result[1]]);
+        } else {
+            notify(['setUserId', '']);
+        }
+    });
 };
 
 // Xiaohongshu
-const getXiaohongshu = (value) => {
-  const re = /www\.xiaohongshu\.com\/user\/profile\/(.+?)\?/;
-  const result = value.match(re);
-  if (result) {
-    console.log(result[1]);
-    notify(['setUserId', result[1]]);
-  } else {
-    notify(['setUserId', '']);
-  }
+const getXiaohongshu = (query) => {
+    const re = /www\.xiaohongshu\.com\/user\/profile\/(.+?)\?/;
+    const result = query.match(re);
+    if (result) {
+        console.log(result[1]);
+        notify(['setUserId', result[1]]);
+    } else {
+        notify(['setUserId', '']);
+    }
 };
 
 // Douyin
-const getDouyin = (value) => {
-  if (!value.match(/douyin/)) {
-    notify(['setUserId', '']);
-    return;
-  }
-  fetch(value.split('?', 1)[0], {
-    headers: {
-      'content-type': 'text/html;',
-    },
-  }).then(function(response) {
-    return response.url;
-  }).then(function(text) {
-    const re = /user\/(\d+?)\?/;
-    const result = text.match(re);
-    if (result) {
-      console.log(result[1]);
-      notify(['setUserId', result[1]]);
-    } else {
-      notify(['setUserId', '']);
+const getDouyin = (query) => {
+    if (!query.match(/douyin/)) {
+        notify(['setUserId', '']);
+        return;
     }
-  });
+    fetch(query.split('?', 1)[0], {
+        headers: {
+            'content-type': 'text/html;',
+        },
+    }).then(function(response) {
+        return response.url;
+    }).then(function(text) {
+        const re = /user\/(\d+?)\?/;
+        const result = text.match(re);
+        if (result) {
+            console.log(result[1]);
+            notify(['setUserId', result[1]]);
+        } else {
+            notify(['setUserId', '']);
+        }
+    });
 };
 
 // Wanmeicaizhuang
-const getWanmeicaizhuang = (value) => {
-  const re = /www\.beautycircle\.com\/profile\/(\d+)/;
-  const result = value.match(re);
-  if (result) {
-    console.log(result[1]);
-    notify(['setUserId', result[1]]);
-  } else {
-    notify(['setUserId', '']);
-  }
+const getWanmeicaizhuang = (query) => {
+    const re = /www\.beautycircle\.com\/profile\/(\d+)/;
+    const result = query.match(re);
+    if (result) {
+        console.log(result[1]);
+        notify(['setUserId', result[1]]);
+    } else {
+        notify(['setUserId', '']);
+    }
 };
 
-const getUserId = (type, value) => {
-  console.log(type, value);
-  switch (type) {
-    case 'kuaishou':
-      getKuaishou(value);
-      break;
-    case 'xiaohongshu':
-      getXiaohongshu(value);
-      break;
-    case 'douyin':
-      getDouyin(value);
-      break;
-    case 'wanmeicaizhuang':
-      getWanmeicaizhuang(value);
-      break;
-    default:
-      break;
-  }
+// Guimimeizhuang
+const getGuimimeizhuang = (query) => {
+    let formData = new FormData();
+    formData.append('platform', 2);
+    formData.append('rd', 512);
+    formData.append('page', 1);
+    // formData.append('channel', '小米开发平台');
+    formData.append('keyword', query.trim());
+    fetch('http://misc.kimiss.com/common/mapi/v151/', {
+        method: 'post',
+        body: formData,
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        console.log(json);
+        const result = json.de.thy.map((v) => {
+            return [v.ar, v.ue, v.ud];
+        });
+        if (result) {
+            console.log(result);
+            notify(['setResult', result]);
+        } else {
+            notify(['setResult', []]);
+        }
+    });
+};
+
+// Weimeimeizhuang
+const getWeimeimeizhuang = (query) => {
+    fetch(
+        'https://app.vmei.com/beauty/search/user?pageSize=20&pageNo=1&keyword=' +
+        query.trim(), {}).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        console.log(json);
+        const result = json.data.post.products.map((v) => {
+            return [
+                'http://img06.vmei.com' + v.headIconUrl,
+                v.nickName,
+                v.userId];
+        });
+        if (result) {
+            console.log(result);
+            notify(['setResult', result]);
+        } else {
+            notify(['setResult', []]);
+        }
+    });
+};
+
+const getUserId = (type, query) => {
+    console.log(type, query);
+    switch (type) {
+        case 'kuaishou':
+            getKuaishou(query);
+            break;
+        case 'xiaohongshu':
+            getXiaohongshu(query);
+            break;
+        case 'douyin':
+            getDouyin(query);
+            break;
+        case 'wanmeicaizhuang':
+            getWanmeicaizhuang(query);
+            break;
+        case 'guimimeizhuang':
+            getGuimimeizhuang(query);
+            break;
+        case 'weimeimeizhuang':
+            getWeimeimeizhuang(query);
+            break;
+        default:
+            break;
+    }
 };
 
 window.bg = {
-  getUserId: getUserId,
+    getUserId: getUserId,
 };
--- src/js/popup.js
+++ src/js/popup.js
@@ -3,20 +3,37 @@
 const bg = chrome.extension.getBackgroundPage().bg;
 
 const setUserId = (value) => {
-  const labelUserId = document.querySelector('#user-id');
-  labelUserId.value = value;
+    const labelUserId = document.querySelector('#user-id');
+    labelUserId.value = value;
+};
+
+const setResult = (value) => {
+    value.forEach(
+        (v) => {
+            let node = document.createElement('li');
+            node.innerHTML = '<img src="' + v[0] + '"><span>' + v[1] +
+                '</span><span>' + v[2] + '</span>';
+            node.onclick = () => {
+                setUserId(v[2]);
+            };
+            ulResult.appendChild(node);
+        },
+    );
 };
 
 // ui
 window.$ui = {
-  setUserId: setUserId,
+    setUserId: setUserId,
+    setResult: setResult,
 };
 
 const input = document.querySelectorAll('input');
+const ulResult = document.querySelector('#result');
 input.forEach((v) => {
-  v.onchange = (() => {
-    const inputType = document.querySelector('input[name="type"]:checked');
-    const inputUrl = document.querySelector('#url');
-    bg.getUserId(inputType.value, inputUrl.value);
-  });
+    v.onchange = (() => {
+        ulResult.innerHTML = '';
+        const inputType = document.querySelector('input[name="type"]:checked');
+        const inputQuery = document.querySelector('#query');
+        bg.getUserId(inputType.value, inputQuery.value);
+    });
 });
